4488f6292e87948981b6f6abca24369ea6988dcc
johnbailon <john.l.bailon@gmail.com>
2024-10-22 23:38:17 -0400

    Q 5.2 and 5.3 code + graphs

--- hw2/HW2 Code/year_train.py
+++ hw2/HW2 Code/year_train.py
@@ -8,41 +8,37 @@ import numpy as np
 graph_dir = Path(__file__).resolve().parent.parent / "HW2_data/P5_data"
 
 def pre_process(matrix):
+    # SVD
     centered_matrix = matrix - np.mean(matrix, axis=0)
     covariance = np.cov(centered_matrix, rowvar=False)
     eigenValues, eigenVectors = np.linalg.eigh(covariance)
 
-    inv_sqrt_eigenValues = np.diag(1.0 / np.sqrt(eigenValues))
-    whitened_data = inv_sqrt_eigenValues @ eigenVectors.T @ centered_matrix.T
-    whitened_matrix = whitened_data[(0,1), :].T
+    # Select top 2 PCs
+    idx = eigenValues.argsort()[::-1]   
+    eigenVectors = eigenVectors[:,idx]
+    eigenValues = eigenValues[idx]
+
+    top_pc = 2
+    pca_eigenVectors = eigenVectors[:, :top_pc]
+    pca_eigenValues = eigenValues[:top_pc]
+    proj_pca = centered_matrix @ pca_eigenVectors
+
+    inv_sqrt_eigenValues = np.diag(1.0 / np.sqrt(pca_eigenValues))
+    whitened_matrix = proj_pca @ inv_sqrt_eigenValues
 
     return whitened_matrix
 
-# def construct_data_matrix(matrix, polyDegree):
-#     feature_1= np.column_stack([matrix[:, 0]**i for i in range(polyDegree)])
-#     feature_2= np.column_stack([matrix[:, 1]**i for i in range(1, polyDegree)])
-#     data_matrix = np.hstack((feature_1, feature_2))
+def construct_data_matrix(matrix, polyDegree):
+    feature_1= np.column_stack([matrix[:, 0]**i for i in range(polyDegree)])
+    feature_2= np.column_stack([matrix[:, 1]**i for i in range(1, polyDegree)])
+    data_matrix = np.hstack((feature_1, feature_2))
 
-#     return data_matrix
-
-def construct_data_matrix(X, degree):
-    n_samples, n_features = X.shape
-    poly_matrix = np.ones((n_samples, (degree + 1) * n_features))
-
-    for i in range(n_features):
-        for j in range(1, degree + 1):
-            poly_matrix[:, i * (degree + 1) + j] = X[:, i] ** j
-
-    return poly_matrix
+    return data_matrix
 
 # Load Data
 train_logit=np.load(graph_dir / "vgg16_train.npz", allow_pickle=True)["logit"]
 train_year=np.load(graph_dir / "vgg16_train.npz", allow_pickle=True)["year"]
 train_filename=np.load(graph_dir / "vgg16_train.npz", allow_pickle=True)["filename"] 
-
-test_logit=np.load(graph_dir / "vgg16_test.npz", allow_pickle=True)["logit"]
-test_year=np.load(graph_dir / "vgg16_test.npz", allow_pickle=True)["year"]
-test_filename=np.load(graph_dir / "vgg16_test.npz", allow_pickle=True)["filename"] 
 
 # Pre-process data
 train_logit_processed = pre_process(train_logit)
@@ -54,7 +50,7 @@ whitened_valid_logit = train_logit_processed[5000:]
 sub_train_year = train_year[:5000]
 sub_valid_year = train_year[5000:]
 
-polyDegree = [i for i in range(2, 10)]
+polyDegree = [i for i in range(2, 9)]
 valid_mse_list = []
 
 for degree in polyDegree:
@@ -69,15 +65,13 @@ for degree in polyDegree:
     # Valid MSE
     valid_pred = validation_data_matrix @ weights
     valid_mse = np.mean((sub_valid_year - valid_pred) ** 2)
-
-    # train_mse_list.append(valid_pred)
     valid_mse_list.append(valid_mse)
 
 polyDegree = [i-1 for i in polyDegree]
 
-plt.scatter(polyDegree, valid_mse_list, color='red', label='Valid')
+plt.scatter(polyDegree, valid_mse_list, color='red', label='Validation MSE')
 
 # Graph info
-plt.title('')
+plt.title('Polynomial Basis vs MSE')
 plt.legend()
 plt.show()
