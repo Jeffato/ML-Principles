acda9c5d742e6d660998dbb93923b98147a9320b
johnbailon <john.l.bailon@gmail.com>
2024-11-17 20:37:11 -0500

    Q3.4

--- hw3/HW3_Code/train3_4.py
+++ hw3/HW3_Code/train3_4.py
@@ -16,15 +16,17 @@ y_df = pd.DataFrame(train_y, columns=['Result'])
 pos = x_df[y_df['Result'] == 1]
 neg = x_df[y_df['Result'] == -1]
 
-print(pos.shape)
-print(neg.shape)
+# Mean and Cov for pos/neg
+mean_pos = pos.mean()
+mean_neg = neg.mean()
 
-# Mean and Variance for pos/neg
-mean_pos = pos.mean().mean()
-mean_neg = neg.mean().mean()
+#COV
+cov_pos  = np.cov(pos, rowvar = False)
+cov_neg = np.cov(neg, rowvar = False)
 
-var_pos = pos.var().mean()
-var_neg = neg.var().mean()
+#Prior values: Going to be saved alongside mean and variance for 3.3
+prior_pos = np.mean(train_y==1)
+prior_neg = np.mean(train_y==-1)
 
-print(f'Positive Set: Mean {mean_pos}, Variance: {var_pos}')
-print(f'Negative Set: Mean {mean_neg}, Variance: {var_neg}')
+print(f'Positive Set: Mean: Class 1: {mean_pos[0]}, Mean: Class 2: {mean_pos[1]} Covariance: {cov_pos}, Prior: {prior_pos}')
+print(f'Negative Set: Mean: Class 1: {mean_neg[0]}, Mean: Class 2: {mean_neg[1]} Covariance: {cov_neg}, Prior: {prior_neg}')
